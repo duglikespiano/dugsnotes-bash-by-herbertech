bfbd94e1ecb1445ab4dd5b692b43da4b6d7a9691
SUHO PARK <duglikespiano@gmail.com>
2026-02-07 18:22:54 +0900

    19 - IF STATEMENT

---
+++ 19-if-statement.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME="Dug"
+
+if [ "$NAME" == "Dug" ]
+then 
+  echo "Your name is Dug"
+elif [ "$NAME" == "Doooog" ]
+then
+  echo "Your name is Doooog"
+else
+  echo "Your name is NOT Dug or Doooog"
+fi
