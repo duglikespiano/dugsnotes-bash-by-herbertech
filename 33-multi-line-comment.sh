3c5801346d91bdc8310630f815a5de992dbc9c90
SUHO PARK <duglikespiano@gmail.com>
2026-02-22 13:54:23 +0900

    33 - MULTI LINE COMMENT

---
+++ 33-multi-line-comment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+: '
+This is multi line comment
+This is multi line comment
+This is multi line comment
+This is multi line comment
+This is multi line comment
+This is multi line comment
+This is multi line comment
+This is multi line comment
+This is multi line comment
+'
